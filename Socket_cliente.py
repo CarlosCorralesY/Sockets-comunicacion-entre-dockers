2159fa86babbe2087fdb33c98145aa56960b706f
Carlos Corrales Yarasca <ccorralesy@ulasalle.edu.pe>
2022-05-08 22:29:16 -0500

    cliente en envio, data en utf-8, tmaño de la data

--- Socket_cliente.py
+++ Socket_cliente.py
@@ -4,3 +4,11 @@ PORT = 8080
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER,PORT))
 client.sendall(bytes("Yo soy el cliente",'UTF-8'))
+while True:
+    in_data = client.recv(1024)
+    print ("Desde el server:", in_data.decode())
+    out_data = input()
+    client.sendall(bytes(out_data,'UTF-8'))
+    if out_data == 'bye':
+        break
+client.close()
