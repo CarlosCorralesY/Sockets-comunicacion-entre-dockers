4f044044510f988d3efe7f6ee9b56586d24b735d
Carlos Corrales Yarasca <ccorralesy@ulasalle.edu.pe>
2022-05-08 21:39:08 -0500

    se crean mensajes de conexion

--- sockets_server.py
+++ sockets_server.py
@@ -3,4 +3,9 @@ import threading
 
 class HiloCliente(threading.Thread):
     def _init_(self,ipCliente,clienteSocket):
+       threading.Thread._init_(self)
+       self.csocket = clienteSocket
+       print("Nueva conexion",ipCliente)
+    def run(self):
+        print ("Conexion desde: ",ipCliente) 
         
