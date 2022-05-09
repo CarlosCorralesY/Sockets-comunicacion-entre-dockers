4b80e7e5f4cca616e6a93ab8227ffe0fc3bae9ff
Carlos Corrales Yarasca <ccorralesy@ulasalle.edu.pe>
2022-05-08 22:11:39 -0500

    Se crean Hilos, server en modo escucha

--- sockets_server.py
+++ sockets_server.py
@@ -22,3 +22,11 @@ PORT = 8080
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
 server.bind ((LOCALHOST,PORT))
+print ("Server Iniciado")
+print ("Esperando Cliente")
+while True:
+    server.listen(1)
+    clienteSock,ipCliente = server.accept()
+    newthread = clienteThread(ipCliente, clienteSock)
+    newthread.start()
+    
