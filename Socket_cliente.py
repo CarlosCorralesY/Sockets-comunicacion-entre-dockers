2ff5737d6d772b53d627f836a3dae8a53d255f7f
Carlos Corrales Yarasca <ccorralesy@ulasalle.edu.pe>
2022-05-08 22:23:59 -0500

    Iniciamos Cliente y enviamos mensaje

--- Socket_cliente.py
+++ Socket_cliente.py
@@ -1,3 +1,6 @@
 import socket
 SERVER = "Direccion Server"
 PORT = 8080
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER,PORT))
+client.sendall(bytes("Yo soy el cliente",'UTF-8'))
